373c868358d9a6f2ba0fb66a3bb67b99dd2c876f
unknown <seidukinasua@gmail.com>
2025-11-13 21:45:01 +0000

    Remove sensitive data

--- server/src/routes/newsletter.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { subscribeNewsletter } from "../controllers/newsletter.controller";
-
-const router = Router();
-
-router.post("/", subscribeNewsletter);
-
-export default router;
-
--- server/src/types/express-mongo-sanitize.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "express-mongo-sanitize" {
-  import { Request, RequestHandler } from "express";
-  function mongoSanitize(options?: { onSanitize?: (req: Request, key: string) => void }): RequestHandler;
-  export default mongoSanitize;
-}
-
